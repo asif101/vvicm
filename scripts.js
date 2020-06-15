f30c1315ffe6cf8fd6adfe72ee8c31d73844a0ae
Joseph Conroy <joey.conroy@jhuapl.edu>
2020-06-14 20:12:48 -0400

    trying

--- scripts.js
+++ scripts.js
@@ -1,47 +1,10 @@
-document.addEventListener('click', function(e) {
-  
-  
-  if (e.target.tagName !== 'A') return;
-  
-  if ((e.target.href && e.target.href.indexOf('#') != -1) && ((e.target.pathname == location.pathname) || ('/' + e.target.pathname == location.pathname)) && (e.target.search == location.search)) {
-			scrollAnchors(e, e.target); 
-  }  
-});
-
-	
-function scrollAnchors(e, respond = null) {
-	// const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-
-function distanceToTop(el) { 
-	return Math.floor(el.getBoundingClientRect().top); 
-}
-
-e.preventDefault();
-var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-var targetAnchor = document.querySelector(targetID);
-if (!targetAnchor) return;
-var originalTop = distanceToTop(targetAnchor);
-window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-var checkIfDone = setInterval(function() {
-	var atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-	if (distanceToTop(targetAnchor) === 0 || atBottom) {
-		targetAnchor.tabIndex = '-1';
-		targetAnchor.focus();
-
-if ('history' in window) {
-
-			window.history.pushState('', '', targetID);
-
-} else {
-window.location = targetID;
+function scrollTo(el) {
+	target = document.getElementById(el)
+	target.scrollIntoView({
+		behavior: "smooth"
+	});
 
 }
-
-		clearInterval(checkIfDone);
-	}
-}, 100);
-}
-
 
 //Dismiss alert banner
 function dismissAlert() {
